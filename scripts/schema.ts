d60d6e06656cdca95b8c85cf32274a3cfa80b65a
Joe Johnston <104495+simple10@users.noreply.github.com>
2025-03-06 17:59:39 -0800

    Add schema script runner

---
+++ scripts/schema.ts
@@ -0,0 +1,50 @@
+import { createServiceSchema, removeServiceSchema } from './lib/postgres.ts'
+import { ALL_COMPOSE_SERVICES, loadEnv, showAction, showError, showInfo } from './start.ts'
+
+async function run() {
+  // Check if the first argument is start or stop
+  const action = Deno.args[0]
+  // Get the service name from the second argument
+  const service = Deno.args[1]
+
+  if (action !== 'create' && action !== 'remove') {
+    showError('First argument must be either "create" or "remove"')
+    Deno.exit(1)
+  }
+  if (!service) {
+    showError('Service name is required')
+    Deno.exit(1)
+  }
+
+  loadEnv()
+
+  // Make sure it's a valid service
+  if (!ALL_COMPOSE_SERVICES.find(([s]) => s === service)) {
+    showError(`Unknown service: ${service}`)
+    Deno.exit(1)
+  }
+
+  const password = Deno.env.get('POSTGRES_PASSWORD') ?? ''
+
+  if (action === 'create') {
+    showAction(`Creating schema for ${service}...`)
+    const credentials = await createServiceSchema(service, {
+      password,
+    })
+    showInfo(`Schema created for ${service}`)
+    showInfo(`Username: ${credentials.username}`)
+    showInfo(`Password: ${credentials.password}`)
+    showInfo(`Database: ${credentials.database}`)
+  } else if (action === 'remove') {
+    showAction(`Removing schema for ${service}...`)
+    await removeServiceSchema(service, {
+      password,
+    })
+    showInfo(`Schema removed for ${service}`)
+  }
+}
+
+// Run script if this file is executed directly
+if (import.meta.main) {
+  run()
+}
