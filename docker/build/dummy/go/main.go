0c50c9f93a2ef54649025ec2812d169d977972e4
Joe Johnston <104495+simple10@users.noreply.github.com>
2025-03-15 01:49:51 -0700

    Switch to the golang dummy analytics server
    Less than 3mb of memory and portable! The container only includes the dummy go server.

--- docker/build/dummy/go/main.go
+++ docker/build/dummy/go/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,6 +13,19 @@ import (
 var requestCount uint64 = 0
 
 func main() {
+	// Parse flags
+	healthCheck := flag.Bool("health-check", false, "Run a health check and exit")
+	flag.Parse()
+
+	// If this is a health check request, just check health and exit
+	if *healthCheck {
+		resp, err := http.Get("http://localhost:4000/health")
+		if err != nil || resp.StatusCode != http.StatusOK {
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "4000"
@@ -24,13 +38,13 @@ func main() {
 
 	// Start server
 	addr := fmt.Sprintf("0.0.0.0:%s", port)
-	fmt.Printf("LOGFLARE-SINK: Starting server on %s\n", addr)
+	fmt.Printf("DUMMY LOG SERVER: Starting server on %s\n", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
 func handleAPI(w http.ResponseWriter, r *http.Request) {
 	count := atomic.AddUint64(&requestCount, 1)
-	fmt.Printf("LOGFLARE-SINK: Request #%d from IP %s - %s %s\n",
+	fmt.Printf("LOG: #%d from IP %s - %s %s\n",
 		count,
 		r.RemoteAddr,
 		r.Method,
@@ -42,7 +56,7 @@ func handleAPI(w http.ResponseWriter, r *http.Request) {
 
 func handleLogs(w http.ResponseWriter, r *http.Request) {
 	count := atomic.AddUint64(&requestCount, 1)
-	fmt.Printf("LOGFLARE-SINK: Request #%d from IP %s - %s /logs\n",
+	fmt.Printf("LOG: #%d from IP %s - %s /logs\n",
 		count,
 		r.RemoteAddr,
 		r.Method)
