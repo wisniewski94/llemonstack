080605678af9bc852a08461e0c53ed4d1b3a4f27
Joe Johnston <104495+simple10@users.noreply.github.com>
2025-03-24 14:21:18 -0700

    Show nicer error message with n8n export

--- scripts/n8n_export.ts
+++ scripts/n8n_export.ts
@@ -3,10 +3,15 @@
  * Export workflows from n8n
  */
 
+import { CommandError } from './lib/command.ts'
+import { Config } from './lib/config/config.ts'
 import { dockerExec } from './lib/docker.ts'
 import { fs, path } from './lib/fs.ts'
-import { showAction, showInfo, showWarning } from './lib/logger.ts'
-import { DEFAULT_PROJECT_NAME, getComposeFile, isEnabled } from './start.ts'
+import { showAction, showError, showInfo, showWarning } from './lib/logger.ts'
+import { DEFAULT_PROJECT_NAME } from './start.ts'
+
+const config = Config.getInstance()
+await config.initialize()
 
 // Relative path of backup dir in shared dir
 const BACKUP_DIR_N8N = 'backups/n8n'
@@ -15,6 +20,7 @@ const SERVICES_EXPORT_COMMANDS: ServiceExportCommand[] = [
   // Export workflows from n8n to the shared folder
   {
     service: 'n8n',
+    type: 'workflows',
     hostDir: `shared/${BACKUP_DIR_N8N}/workflows`,
     cmd: 'n8n',
     args: ['export:workflow', '--backup', '--output', `/data/shared/${BACKUP_DIR_N8N}/workflows`],
@@ -22,6 +28,7 @@ const SERVICES_EXPORT_COMMANDS: ServiceExportCommand[] = [
   // Export credentials from n8n to the shared folder
   {
     service: 'n8n',
+    type: 'credentials',
     hostDir: `shared/${BACKUP_DIR_N8N}/credentials`,
     cmd: 'n8n',
     args: [
@@ -36,6 +43,7 @@ const SERVICES_EXPORT_COMMANDS: ServiceExportCommand[] = [
 
 interface ServiceExportCommand {
   service: string
+  type: 'workflows' | 'credentials'
   cmd: string
   args: string[]
   hostDir: string // Directory to export to on host
@@ -54,16 +62,27 @@ async function prepareBackupDir(): Promise<BackupDirs> {
 
 async function runExportCommands(projectName: string): Promise<void> {
   for (const serviceCommand of SERVICES_EXPORT_COMMANDS) {
-    const composeFile = getComposeFile(serviceCommand.service)
+    const composeFile = config.getComposeFile(serviceCommand.service)
     if (!composeFile) {
-      isEnabled(serviceCommand.service) &&
+      config.isEnabled(serviceCommand.service) &&
         showWarning(`Compose file not found for ${serviceCommand.service}, skipping`)
       continue
     }
-    await dockerExec(projectName, serviceCommand.service, serviceCommand.cmd, {
-      args: serviceCommand.args,
-      composeFile,
-    })
+    try {
+      await dockerExec(projectName, serviceCommand.service, serviceCommand.cmd, {
+        args: serviceCommand.args,
+        composeFile,
+        captureOutput: true,
+      })
+    } catch (error) {
+      if (error instanceof CommandError) {
+        if (!error.stderr.match(/no (workflows|credentials) found/ig)) {
+          showInfo(`No ${serviceCommand.type} found to export.\n`)
+        }
+      } else {
+        showError(`Error exporting ${serviceCommand.service}`, error)
+      }
+    }
   }
 }
 export async function runExport(projectName: string): Promise<void> {
@@ -71,8 +90,12 @@ export async function runExport(projectName: string): Promise<void> {
   const dirs = await prepareBackupDir()
   showInfo(`Export dirs:\n  ${dirs.map((d) => d[1]).join('\n  ')}`)
   showAction(`Exporting n8n workflows and credentials...`)
-  await runExportCommands(projectName)
-  showInfo(`Export complete`)
+  try {
+    await runExportCommands(projectName)
+    showInfo(`Export complete`)
+  } catch (error) {
+    showError(`Error exporting n8n workflows and credentials`, error)
+  }
 }
 
 // Run script if this file is executed directly
