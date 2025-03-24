39a41e816dfb1d5bde4c1e4a18f05715f936ff45
Joe Johnston <104495+simple10@users.noreply.github.com>
2025-03-23 19:04:45 -0700

    Stub out Service lib

---
+++ scripts/lib/service.ts
@@ -0,0 +1,53 @@
+/**
+ * Service class
+ *
+ * Services can provide a service.ts file to override the default behavior.
+ * The service.ts file should export a Service class that extends the base Service class.
+ */
+import { Config } from './config.bak.ts'
+
+// Use decorators for logger, config, etc.
+export class Service {
+  // @Config()
+  private readonly config: Config
+
+  constructor(private readonly _config: Config) {
+    this.config = _config
+  }
+
+  public init() {
+    console.log('Initializing service')
+  }
+
+  public start() {
+    console.log('Starting service')
+  }
+
+  public stop() {
+    console.log('Stopping service')
+  }
+
+  public update() {
+    console.log('Updating service')
+  }
+
+  public version() {
+    console.log('Version of service')
+  }
+
+  public import() {
+    console.log('Importing service')
+  }
+
+  public export() {
+    console.log('Exporting service')
+  }
+
+  // public backup() {
+  //   console.log('Backing up service')
+  // }
+
+  // public restore() {
+  //   console.log('Restoring service')
+  // }
+}
