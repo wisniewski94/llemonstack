8f165d30b7bea6a9abd7a2b3ee7f7b5ffe65a251
Joe Johnston <104495+simple10@users.noreply.github.com>
2025-03-24 10:37:43 -0700

    Stub out more of the services API

--- scripts/lib/service.ts
+++ scripts/lib/service.ts
@@ -5,12 +5,34 @@
  * The service.ts file should export a Service class that extends the base Service class.
  */
 import { Config } from './config/config.ts'
+// import { Logger } from './logger.ts'
+interface ServiceConfig {
+  config: Config
+  // logger: Logger
+  // llemonstackApi: {} // Provides interface to access other services like create schema?
+  services: Array<{
+    stackHost: 'service:port'
+    hostPort: '1234' // Host port exposed by service
+    description: 'Service description'
+    credentials: {
+      username: 'username'
+      password: 'password'
+      apiKey: 'apiKey'
+    }
+  }>
+}
+
+// interface ServiceStartResult {
+//   // List of messages to output to show startup status
+//   // messages: Array<LoggerFunction>
+// }
 
 // Use decorators for logger, config, etc.
 export class Service {
   // @Config()
   private readonly config: Config
 
+  // On initialization, service should provide an API that shows exposed ports
   constructor(private readonly _config: Config) {
     this.config = _config
   }
