2f28094c3f64ed2fe0b495cc6e17e7bb1f9ac0df
Joe Johnston <104495+simple10@users.noreply.github.com>
2025-04-20 11:47:46 -0700

    Move supabase JWT to SupabaseService class

---
+++ services/supabase/service.ts
@@ -0,0 +1,14 @@
+const supabaseAnonJWTPayload = {
+  'role': 'anon',
+  'iss': 'supabase',
+  'iat': 1740902400,
+  'exp': 1898668800,
+}
+
+const supabaseServiceJWTPayload = {
+  'role': 'service_role',
+  'iss': 'supabase',
+  'iat': 1740902400,
+  'exp': 1898668800,
+}
+
--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -17,19 +17,19 @@
 import { signJWT } from 'jsr:@cross/jwt'
 import { crypto } from 'jsr:@std/crypto'
 
-export const supabaseAnonJWTPayload = {
-  'role': 'anon',
-  'iss': 'supabase',
-  'iat': 1740902400,
-  'exp': 1898668800,
-}
+// export const supabaseAnonJWTPayload = {
+//   'role': 'anon',
+//   'iss': 'supabase',
+//   'iat': 1740902400,
+//   'exp': 1898668800,
+// }
 
-export const supabaseServiceJWTPayload = {
-  'role': 'service_role',
-  'iss': 'supabase',
-  'iat': 1740902400,
-  'exp': 1898668800,
-}
+// export const supabaseServiceJWTPayload = {
+//   'role': 'service_role',
+//   'iss': 'supabase',
+//   'iat': 1740902400,
+//   'exp': 1898668800,
+// }
 
 /**
  * Generates a cryptographically secure random UUID
